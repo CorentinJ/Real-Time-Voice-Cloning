ab08fe5f49cc5ccbfd9384df52628bf7931507bb
Carlo <carloprovinciali@gmail.com>
2019-11-03 18:09:40 -0500

    working proof of concept

--- demo_cli.py
+++ demo_cli.py
@@ -12,6 +12,69 @@ import sys
 import speech_recognition
 
 
+def clone_voice(in_fpath_1, in_fpath_2, encoder, synthesizer, vocoder, num_generated):
+            ## Computing the embedding
+            # First, we load the wav using the function that the speaker encoder provides. This is 
+            # important: there is preprocessing that must be applied.
+            
+            # The following two methods are equivalent:
+            # - Directly load from the filepath:
+            preprocessed_wav = encoder.preprocess_wav(Path(in_fpath_1))
+            # - If the wav is already loaded:
+            original_wav, sampling_rate = librosa.load(Path(in_fpath_1))
+            preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
+            print("Loaded file succesfully")
+            
+            # Then we derive the embedding. There are many functions and parameters that the 
+            # speaker encoder interfaces. These are mostly for in-depth research. You will typically
+            # only use this function (with its default parameters):
+            embed = encoder.embed_utterance(preprocessed_wav)
+            print("Created the embedding")
+            
+            ##we would probably make a call to the speech to text API here
+            recognizer = speech_recognition.Recognizer()
+            
+            with speech_recognition.AudioFile(in_fpath_2) as source:
+                audio = recognizer.record(source)
+                
+            ## Generating the spectrogram
+            #text = input("Write a sentence (+-20 words) to be synthesized:\n")
+            text = recognizer.recognize_google(audio)
+            # The synthesizer works in batch, so you need to put your data in a list or numpy array
+            texts = [text]
+            embeds = [embed]
+            # If you know what the attention layer alignments are, you can retrieve them here by
+            # passing return_alignments=True
+            specs = synthesizer.synthesize_spectrograms(texts, embeds)
+            spec = specs[0]
+            print("Created the mel spectrogram")
+            
+            
+            ## Generating the waveform
+            print("Synthesizing the waveform:")
+            # Synthesizing the waveform is fairly straightforward. Remember that the longer the
+            # spectrogram, the more time-efficient the vocoder.
+            generated_wav = vocoder.infer_waveform(spec)
+            
+            
+            ## Post-generation
+            # There's a bug with sounddevice that makes the audio cut one second earlier, so we
+            # pad it.
+            generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
+            
+            # Play the audio (non-blocking)
+            if not args.no_sound:
+                sd.stop()
+                sd.play(generated_wav, synthesizer.sample_rate)
+                
+            # Save it on the disk
+            fpath = "demo_output_%02d.wav" % num_generated
+            print(generated_wav.dtype)
+            librosa.output.write_wav(fpath, generated_wav.astype(np.float32), 
+                                     synthesizer.sample_rate)
+            
+            print("\nSaved output as %s\n\n" % fpath)
+            
 if __name__ == '__main__':
     ## Info & args
     parser = argparse.ArgumentParser(
@@ -120,73 +183,20 @@ if __name__ == '__main__':
             # Get the reference audio filepath
             message = "Reference voice: enter an audio filepath of first voice to be swapped  (mp3, " \
                       "wav, m4a, flac, ...):\n"
-            in_fpath = Path(input(message).replace("\"", "").replace("\'", ""))
             
+            in_fpath_1 = input(message).replace("\"", "").replace("\'", "")
             
-            ## Computing the embedding
-            # First, we load the wav using the function that the speaker encoder provides. This is 
-            # important: there is preprocessing that must be applied.
+            message = "Reference voice: enter an audio filepath of second voice to be swapped  (mp3, " \
+                      "wav, m4a, flac, ...):\n"
             
-            # The following two methods are equivalent:
-            # - Directly load from the filepath:
-            preprocessed_wav = encoder.preprocess_wav(in_fpath)
-            # - If the wav is already loaded:
-            original_wav, sampling_rate = librosa.load(in_fpath)
-            preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
-            print("Loaded file succesfully")
+            in_fpath_2 = input(message).replace("\"", "").replace("\'", "")
             
-            # Then we derive the embedding. There are many functions and parameters that the 
-            # speaker encoder interfaces. These are mostly for in-depth research. You will typically
-            # only use this function (with its default parameters):
-            embed = encoder.embed_utterance(preprocessed_wav)
-            print("Created the embedding")
-            
-            ##we would probably make a call to the speech to text API here
-            recognizer = speech_recognition.Recognizer()
-            test_source_path = "C:/Users/Carlo/Desktop/Voice-Swapping-Dataset/LibriSpeech/train-clean-100/19/198/19-198-0006.flac"
-            print(test_source_path)
-            with speech_recognition.AudioFile(test_source_path) as source:
-                audio = recognizer.record(source)
-                
-            ## Generating the spectrogram
-            #text = input("Write a sentence (+-20 words) to be synthesized:\n")
-            text = recognizer.recognize_google(audio)
-            # The synthesizer works in batch, so you need to put your data in a list or numpy array
-            texts = [text]
-            embeds = [embed]
-            # If you know what the attention layer alignments are, you can retrieve them here by
-            # passing return_alignments=True
-            specs = synthesizer.synthesize_spectrograms(texts, embeds)
-            spec = specs[0]
-            print("Created the mel spectrogram")
-            
-            
-            ## Generating the waveform
-            print("Synthesizing the waveform:")
-            # Synthesizing the waveform is fairly straightforward. Remember that the longer the
-            # spectrogram, the more time-efficient the vocoder.
-            generated_wav = vocoder.infer_waveform(spec)
-            
-            
-            ## Post-generation
-            # There's a bug with sounddevice that makes the audio cut one second earlier, so we
-            # pad it.
-            generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
-            
-            # Play the audio (non-blocking)
-            if not args.no_sound:
-                sd.stop()
-                sd.play(generated_wav, synthesizer.sample_rate)
-                
-            # Save it on the disk
-            fpath = "demo_output_%02d.wav" % num_generated
-            print(generated_wav.dtype)
-            librosa.output.write_wav(fpath, generated_wav.astype(np.float32), 
-                                     synthesizer.sample_rate)
+            clone_voice(in_fpath_1, in_fpath_2, encoder, synthesizer, vocoder, num_generated)
             num_generated += 1
-            print("\nSaved output as %s\n\n" % fpath)
+            clone_voice(in_fpath_2, in_fpath_1, encoder, synthesizer, vocoder, num_generated)
+            num_generated += 1
             
-            
+            break
         except Exception as e:
             print("Caught exception: %s" % repr(e))
             print("Restarting\n")
