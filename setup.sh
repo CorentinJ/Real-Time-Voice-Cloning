04533aafe98fccad0216ffa60d0659d1124aa0b3
pusalieth <johnpring09@gmail.com>
2020-04-30 18:21:31 -0700

    linux setup script working from scratch

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 conda_installed=$(conda list | grep 'conda: command not found')
-if [ $conda_installed != '' ]; then
-    wget -nc https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o ~/Downloads
-    chmod +x ~/Downloads/Miniconda3-latest-Linux-x86_64.sh
-    ./Miniconda3-latest-Linux-x86_64.sh
-    conda create -n RTVC python
-    conda activate RTVC
+if [ '$conda_installed' != '' ]; then
+    wget -nc https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    chmod +x Miniconda3-latest-Linux-x86_64.sh
+    Miniconda3-latest-Linux-x86_64.sh
+    mv Miniconda3-latest-Linux-x86_64.sh ~/Downloads
 fi
 
-sudo apt -y install python3 python3-dev build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev python-pip libportaudio2
-conda update
-conda install pytorch -c pytorch
+conda install pytorch
 python3.7 -m pip install -r requirements.txt
+sudo apt -y install libportaudio2
 echo "Finished installation"
 
-# Fix for "This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem."
+# Possible fix for "This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem."
 # sudo apt-get install libxkbcommon-x11-dev
+
+## Possible fix for webrtcvad failure
+#sudo apt install python3 python3-dev build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev python-pip
