a8f0781498cbe4989a708ec8acf34d4b3b2fb50d
pusalieth <johnpring09@gmail.com>
2020-05-02 00:41:02 -0700

    initializing for rocm support (amd rnn)

--- setup.sh
+++ setup.sh
@@ -9,9 +9,28 @@ fi
 conda install pytorch
 python3.7 -m pip install -r requirements.txt
 sudo apt -y install libportaudio2
+
+## Future AMD setup (needs tensorflow api v2)
+amd='FALSE'
+if [ $amd == 'TRUE' ]; then
+    sudo apt update
+    sudo apt -y dist-upgrade
+    sudo apt install libnuma-dev
+    sudo reboot
+
+    wget -q -O - https://repo.radeon.com/rocm/apt/debian/rocm.gpg.key | sudo apt-key add -
+    echo 'deb [arch=amd64] https://repo.radeon.com/rocm/apt/debian/ xenial main' | sudo tee /etc/apt/sources.list.d/rocm.list
+    sudo apt update
+    sudo apt install rocm-dkms
+    sudo usermod -a -G video $LOGNAME
+    echo 'ADD_EXTRA_GROUPS=1' | sudo tee -a /etc/adduser.conf
+    echo 'EXTRA_GROUPS=video' | sudo tee -a /etc/adduser.conf
+    sudo reboot
+fi
+
 echo "Finished installation"
 
-# Possible fix for "This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem."
+## Possible fix for "This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem."
 # sudo apt-get install libxkbcommon-x11-dev
 
 ## Possible fix for webrtcvad failure
