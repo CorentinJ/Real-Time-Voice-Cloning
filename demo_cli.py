9553eaa1748cf94814be322ec7b096d2d6bc7f28
blue-fish <67130644+blue-fish@users.noreply.github.com>
2020-07-10 08:47:01 -0700

    Catch PortAudioError exception so it is not fatal (#417)

--- demo_cli.py
+++ demo_cli.py
@@ -170,8 +170,14 @@ if __name__ == '__main__':
             
             # Play the audio (non-blocking)
             if not args.no_sound:
-                sd.stop()
-                sd.play(generated_wav, synthesizer.sample_rate)
+                try:
+                    sd.stop()
+                    sd.play(generated_wav, synthesizer.sample_rate)
+                except sd.PortAudioError as e:
+                    print("\nCaught exception: %s" % repr(e))
+                    print("Continuing without audio playback. Suppress this message with the \"--no_sound\" flag.\n")
+                except:
+                    raise
                 
             # Save it on the disk
             filename = "demo_output_%02d.wav" % num_generated
