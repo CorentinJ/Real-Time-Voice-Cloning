c5b192106321ff23e18f7eb9eb8a31cc69019241
blue-fish <67130644+blue-fish@users.noreply.github.com>
2020-08-08 19:17:22 -0700

    Code style update for hparams

--- synthesizer_pt/hparams.py
+++ synthesizer_pt/hparams.py
@@ -28,7 +28,7 @@ tts_lstm_dims = 512
 tts_postnet_K = 8
 tts_num_highways = 4
 tts_dropout = 0.5
-tts_cleaner_names = ['english_cleaners']
+tts_cleaner_names = ["english_cleaners"]
 tts_stop_threshold = -3.4           # Value below which audio generation ends.
                                     # For example, for a range of [-4, 4], this
                                     # will terminate the sequence at the first
