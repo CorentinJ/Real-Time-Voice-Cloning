1ccff2b0a890e2da2892e131d0ab96975087dff9
blue-fish <67130644+blue-fish@users.noreply.github.com>
2020-08-08 18:57:22 -0700

    Code style update for utils

--- synthesizer_pt/utils/display.py
+++ synthesizer_pt/utils/display.py
@@ -1,5 +1,5 @@
 import matplotlib as mpl
-mpl.use('agg')  # Use non-interactive backend by default
+mpl.use("agg")  # Use non-interactive backend by default
 import matplotlib.pyplot as plt
 import time
 import numpy as np
@@ -8,25 +8,21 @@ import sys
 
 def progbar(i, n, size=16):
     done = (i * size) // n
-    bar = ''
+    bar = ""
     for i in range(size):
-        bar += '█' if i <= done else '░'
+        bar += "█" if i <= done else "░"
     return bar
-
 
 def stream(message):
     sys.stdout.write(f"\r{message}")
 
-
 def simple_table(item_tuples):
-
-    border_pattern = '+---------------------------------------'
-    whitespace = '                                            '
+    border_pattern = "+---------------------------------------"
+    whitespace = "                                            "
 
     headings, cells, = [], []
 
     for item in item_tuples:
-
         heading, cell = str(item[0]), str(item[1])
 
         pad_head = True if len(heading) < len(cell) else False
@@ -34,8 +30,8 @@ def simple_table(item_tuples):
         pad = abs(len(heading) - len(cell))
         pad = whitespace[:pad]
 
-        pad_left = pad[:len(pad)//2]
-        pad_right = pad[len(pad)//2:]
+        pad_left = pad[:len(pad) // 2]
+        pad_right = pad[len(pad) // 2:]
 
         if pad_head:
             heading = pad_left + heading + pad_right
@@ -45,29 +41,27 @@ def simple_table(item_tuples):
         headings += [heading]
         cells += [cell]
 
-    border, head, body = '', '', ''
+    border, head, body = "", "", ""
 
     for i in range(len(item_tuples)):
-
-        temp_head = f'| {headings[i]} '
-        temp_body = f'| {cells[i]} '
+        temp_head = f"| {headings[i]} "
+        temp_body = f"| {cells[i]} "
 
         border += border_pattern[:len(temp_head)]
         head += temp_head
         body += temp_body
 
         if i == len(item_tuples) - 1:
-            head += '|'
-            body += '|'
-            border += '+'
+            head += "|"
+            body += "|"
+            border += "+"
 
     print(border)
     print(head)
     print(border)
     print(body)
     print(border)
-    print(' ')
-
+    print(" ")
 
 def time_since(started):
     elapsed = time.time() - started
@@ -76,46 +70,41 @@ def time_since(started):
     if m >= 60:
         h = int(m // 60)
         m = m % 60
-        return f'{h}h {m}m {s}s'
+        return f"{h}h {m}m {s}s"
     else:
-        return f'{m}m {s}s'
-
+        return f"{m}m {s}s"
 
 def save_attention(attn, path):
     fig = plt.figure(figsize=(12, 6))
-    plt.imshow(attn.T, interpolation='nearest', aspect='auto')
-    fig.savefig(path.parent/f'{path.stem}.png', bbox_inches='tight')
+    plt.imshow(attn.T, interpolation="nearest", aspect="auto")
+    fig.savefig(path.parent/f"{path.stem}.png", bbox_inches="tight")
     plt.close(fig)
-
 
 def save_spectrogram(M, path, length=None):
     M = np.flip(M, axis=0)
     if length: M = M[:, :length]
     fig = plt.figure(figsize=(12, 6))
-    plt.imshow(M, interpolation='nearest', aspect='auto')
-    fig.savefig(f'{path}.png', bbox_inches='tight')
+    plt.imshow(M, interpolation="nearest", aspect="auto")
+    fig.savefig(f"{path}.png", bbox_inches="tight")
     plt.close(fig)
-
 
 def plot(array):
     mpl.interactive(True)
     fig = plt.figure(figsize=(30, 5))
     ax = fig.add_subplot(111)
-    ax.xaxis.label.set_color('grey')
-    ax.yaxis.label.set_color('grey')
+    ax.xaxis.label.set_color("grey")
+    ax.yaxis.label.set_color("grey")
     ax.xaxis.label.set_fontsize(23)
     ax.yaxis.label.set_fontsize(23)
-    ax.tick_params(axis='x', colors='grey', labelsize=23)
-    ax.tick_params(axis='y', colors='grey', labelsize=23)
+    ax.tick_params(axis="x", colors="grey", labelsize=23)
+    ax.tick_params(axis="y", colors="grey", labelsize=23)
     plt.plot(array)
     mpl.interactive(False)
-
 
 def plot_spec(M):
     mpl.interactive(True)
     M = np.flip(M, axis=0)
     plt.figure(figsize=(18,4))
-    plt.imshow(M, interpolation='nearest', aspect='auto')
+    plt.imshow(M, interpolation="nearest", aspect="auto")
     plt.show()
     mpl.interactive(False)
-
--- synthesizer_pt/utils/symbols.py
+++ synthesizer_pt/utils/symbols.py
@@ -16,13 +16,13 @@ symbols = [_pad, _eos] + list(_characters)
 if False:
     from . import cmudict
 
-    _pad = '_'
-    _punctuation = '!\'(),.:;? '
-    _special = '-'
-    _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    _pad = "_"
+    _punctuation = "!\'(),.:;? "
+    _special = "-"
+    _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
     # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-    _arpabet = ['@' + s for s in cmudict.valid_symbols]
+    _arpabet = ["@" + s for s in cmudict.valid_symbols]
 
     # Export all symbols:
     symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
