d59ad71be90eefa5b8435507154cc718b3d42f39
blue-fish <67130644+blue-fish@users.noreply.github.com>
2020-08-29 17:53:38 -0700

    Improvements to synthesizer HParams class

--- synthesizer/hparams.py
+++ synthesizer/hparams.py
@@ -1,30 +1,22 @@
 import ast
 import pprint
 
-
 class HParams(object):
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-    def __setitem__(self, key, value):
-        setattr(self, key, value)
-
-    def __getitem__(self, key):
-        return getattr(self, key)
-
-    def __repr__(self):
-        return pprint.pformat(self.__dict__)
+    def __init__(self, **kwargs): self.__dict__.update(kwargs)
+    def __setitem__(self, key, value): setattr(self, key, value)
+    def __getitem__(self, key): return getattr(self, key)
+    def __repr__(self): return pprint.pformat(self.__dict__)
 
     def parse(self, string):
         # Overrides hparams from a comma-separated string of name=value pairs
         if len(string) > 0:
             overrides = [s.split("=") for s in string.split(",")]
             keys, values = zip(*overrides)
+            keys = list(map(str.strip, keys))
+            values = list(map(str.strip, values))
             for k in keys:
                 self.__dict__[k] = ast.literal_eval(values[keys.index(k)])
-
         return self
-
 
 hparams = HParams(
         ### Signal Processing (used in both synthesizer and vocoder)
@@ -95,5 +87,5 @@ hparams = HParams(
         utterance_min_duration = 1.6,               # Duration in seconds below which utterances are discarded
         )
 
-def hparams_debug_string(hparams=hparams):
+def hparams_debug_string():
     return str(hparams)
