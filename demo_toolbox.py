c344e4dd7be562636130548290771c73072221ec
blue-fish <67130644+blue-fish@users.noreply.github.com>
2020-10-15 22:48:44 -0700

    Remove reference to "UserAudio" custom dataset feature for toolbox

--- demo_toolbox.py
+++ demo_toolbox.py
@@ -13,10 +13,7 @@ if __name__ == '__main__':
     
     parser.add_argument("-d", "--datasets_root", type=Path, help= \
         "Path to the directory containing your datasets. See toolbox/__init__.py for a list of "
-        "supported datasets. You can add your own data by created a directory named UserAudio "
-        "in your datasets root. Supported formats are mp3, flac, wav and m4a. Each speaker should "
-        "be inside a directory, e.g. <datasets_root>/UserAudio/speaker_01/audio_01.wav.",
-                        default=None)
+        "supported datasets.", default=None)
     parser.add_argument("-e", "--enc_models_dir", type=Path, default="encoder/saved_models", 
                         help="Directory containing saved encoder models")
     parser.add_argument("-s", "--syn_models_dir", type=Path, default="synthesizer/saved_models", 
