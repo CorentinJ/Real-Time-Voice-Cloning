34b13c267f784fd21f6d799c3feadcc64b676a56
Tomcattwo <colt.coltrain@gmail.com>
2021-09-01 12:38:05 -0400

    Update vocoder_preprocess.py
    @tomcattwo changed line 47 per recommendation by @blue-fish in issue #833; allows --cpu argument to be properly recognized

--- vocoder_preprocess.py
+++ vocoder_preprocess.py
@@ -43,7 +43,8 @@ if __name__ == "__main__":
 
     if args.cpu:
         # Hide GPUs from Pytorch to force CPU processing
-        os.environ["CUDA_VISIBLE_DEVICES"] = ""
+        #@tomcattwo changed line 47 per recommendation by @blue-fish in issue #833; allows --cpu argument to be properly recognized
+        os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
     
     # Verify webrtcvad is available
     if not args.no_trim:
