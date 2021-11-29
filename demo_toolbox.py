f9876ad89e36cb06c723afcbdb67751c45706a2d
sveneschlbeck <sven.eschlbeck@t-online.de>
2021-11-29 21:32:34 +0100

    Added docstring to explain content and ran module against black to format it suitable for CI or infrastructure purposes later to maybe come

--- demo_toolbox.py
+++ demo_toolbox.py
@@ -1,3 +1,12 @@
+"""
+Demo Toolbox
+============
+
+This module contains the commands and parser arguments necessary
+to run the Toolbox in the intended way. The Toolbox is a GUI simplifying
+the interaction with the code's functionality.
+"""
+
 from pathlib import Path
 from toolbox import Toolbox
 from utils.argutils import print_args
@@ -5,28 +14,66 @@ from utils.modelutils import check_model_paths
 import argparse
 import os
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Runs the toolbox",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        description="Runs the Toolbox",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    
-    parser.add_argument("-d", "--datasets_root", type=Path, help= \
-        "Path to the directory containing your datasets. See toolbox/__init__.py for a list of "
-        "supported datasets.", default=None)
-    parser.add_argument("-e", "--enc_models_dir", type=Path, default="encoder/saved_models", 
-                        help="Directory containing saved encoder models")
-    parser.add_argument("-s", "--syn_models_dir", type=Path, default="synthesizer/saved_models", 
-                        help="Directory containing saved synthesizer models")
-    parser.add_argument("-v", "--voc_models_dir", type=Path, default="vocoder/saved_models", 
-                        help="Directory containing saved vocoder models")
-    parser.add_argument("--cpu", action="store_true", help=\
-        "If True, processing is done on CPU, even when a GPU is available.")
-    parser.add_argument("--seed", type=int, default=None, help=\
-        "Optional random number seed value to make toolbox deterministic.")
-    parser.add_argument("--no_mp3_support", action="store_true", help=\
-        "If True, no mp3 files are allowed.")
+
+    # Pass optional arguments to the parser
+    parser.add_argument(
+        "-d",
+        "--datasets_root",
+        type=Path,
+        help="Path to the directory containing your datasets. See toolbox/__init__.py for a list of "
+        "supported datasets.",
+        default=None,
+    )
+
+    parser.add_argument(
+        "-e",
+        "--enc_models_dir",
+        type=Path,
+        default="encoder/saved_models",
+        help="Directory containing saved encoder models",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--syn_models_dir",
+        type=Path,
+        default="synthesizer/saved_models",
+        help="Directory containing saved synthesizer models",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--voc_models_dir",
+        type=Path,
+        default="vocoder/saved_models",
+        help="Directory containing saved vocoder models",
+    )
+
+    parser.add_argument(
+        "--cpu",
+        action="store_true",
+        help="If True, processing is done on CPU, even if a GPU is available.",
+    )
+
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=None,
+        help="Optional random number seed value to make toolbox deterministic.",
+    )
+
+    parser.add_argument(
+        "--no_mp3_support",
+        action="store_true",
+        help="If True, mp3 files are not allowed.",
+    )
+
+    # Parse given arguments
     args = parser.parse_args()
     print_args(args, parser)
 
@@ -35,9 +82,12 @@ if __name__ == '__main__':
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
     del args.cpu
 
-    ## Remind the user to download pretrained models if needed
-    check_model_paths(encoder_path=args.enc_models_dir, synthesizer_path=args.syn_models_dir,
-                      vocoder_path=args.voc_models_dir)
+    ## Remind user to download pretrained models if needed
+    check_model_paths(
+        encoder_path=args.enc_models_dir,
+        synthesizer_path=args.syn_models_dir,
+        vocoder_path=args.voc_models_dir,
+    )
 
     # Launch the toolbox
-    Toolbox(**vars(args))    
+    Toolbox(**vars(args))
