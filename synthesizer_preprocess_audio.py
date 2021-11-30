995a536d4dae6c1558afe05d9721df19245b4725
sveneschlbeck <sven.eschlbeck@t-online.de>
2021-11-30 20:39:05 +0100

    Reformatted the spa module

--- synthesizer_preprocess_audio.py
+++ synthesizer_preprocess_audio.py
@@ -1,38 +1,92 @@
+"""
+Synthesizer Preprocess Audio
+============================
+
+Preprocesses the audio files from datasets to then encode them
+as mel spectrograms and write them to the disk.
+Audio files are saved, too and later used by the vocoder for
+training.
+"""
+
 from synthesizer.preprocess import preprocess_dataset
 from synthesizer.hparams import hparams
 from utils.argutils import print_args
 from pathlib import Path
 import argparse
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Preprocesses audio files from datasets, encodes them as mel spectrograms "
-                    "and writes them to  the disk. Audio files are also saved, to be used by the "
-                    "vocoder for training.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        description="Preprocesses audio files from datasets, encoding them as mel spectrograms "
+        "and writing them to disk. Audio files are also saved to be used by the "
+        "vocoder for training.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("datasets_root", type=Path, help=\
-        "Path to the directory containing your LibriSpeech/TTS datasets.")
-    parser.add_argument("-o", "--out_dir", type=Path, default=argparse.SUPPRESS, help=\
-        "Path to the output directory that will contain the mel spectrograms, the audios and the "
-        "embeds. Defaults to <datasets_root>/SV2TTS/synthesizer/")
-    parser.add_argument("-n", "--n_processes", type=int, default=None, help=\
-        "Number of processes in parallel.")
-    parser.add_argument("-s", "--skip_existing", action="store_true", help=\
-        "Whether to overwrite existing files with the same name. Useful if the preprocessing was "
-        "interrupted.")
-    parser.add_argument("--hparams", type=str, default="", help=\
-        "Hyperparameter overrides as a comma-separated list of name-value pairs")
-    parser.add_argument("--no_trim", action="store_true", help=\
-        "Preprocess audio without trimming silences (not recommended).")
-    parser.add_argument("--no_alignments", action="store_true", help=\
-        "Use this option when dataset does not include alignments\
-        (these are used to split long audio files into sub-utterances.)")
-    parser.add_argument("--datasets_name", type=str, default="LibriSpeech", help=\
-        "Name of the dataset directory to process.")
-    parser.add_argument("--subfolders", type=str, default="train-clean-100, train-clean-360", help=\
-        "Comma-separated list of subfolders to process inside your dataset directory")
+
+    parser.add_argument(
+        "datasets_root",
+        type=Path,
+        help="Path to the directory containing your LibriSpeech/TTS datasets.",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--out_dir",
+        type=Path,
+        default=argparse.SUPPRESS,
+        help="Path to the output directory that will contain the mel spectrograms, the audios and the "
+        "embeds. Defaults to <datasets_root>/SV2TTS/synthesizer/",
+    )
+
+    parser.add_argument(
+        "-n",
+        "--n_processes",
+        type=int,
+        default=None,
+        help="Number of processes in parallel.",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--skip_existing",
+        action="store_true",
+        help="Whether to overwrite existing files with the same name. Useful if the preprocessing was "
+        "interrupted.",
+    )
+
+    parser.add_argument(
+        "--hparams",
+        type=str,
+        default="",
+        help="Hyperparameter overrides as a comma-separated list of name-value pairs",
+    )
+
+    parser.add_argument(
+        "--no_trim",
+        action="store_true",
+        help="Preprocess audio without trimming silences (not recommended).",
+    )
+
+    parser.add_argument(
+        "--no_alignments",
+        action="store_true",
+        help="Use this option when dataset does not include alignments\
+        (these are used to split long audio files into sub-utterances.)",
+    )
+
+    parser.add_argument(
+        "--datasets_name",
+        type=str,
+        default="LibriSpeech",
+        help="Name of the dataset directory to process.",
+    )
+
+    parser.add_argument(
+        "--subfolders",
+        type=str,
+        default="train-clean-100, train-clean-360",
+        help="Comma-separated list of subfolders to process inside your dataset directory",
+    )
+
     args = parser.parse_args()
 
     # Process the arguments
@@ -43,14 +97,16 @@ if __name__ == "__main__":
     assert args.datasets_root.exists()
     args.out_dir.mkdir(exist_ok=True, parents=True)
 
-    # Verify webrtcvad is available
+    # Verify, that webrtcvad is available
     if not args.no_trim:
         try:
             import webrtcvad
         except:
-            raise ModuleNotFoundError("Package 'webrtcvad' not found. This package enables "
+            raise ModuleNotFoundError(
+                "Package 'webrtcvad' not found. This package enables "
                 "noise removal and is recommended. Please install and try again. If installation fails, "
-                "use --no_trim to disable this error message.")
+                "use --no_trim to disable this error message."
+            )
     del args.no_trim
 
     # Preprocess the dataset
