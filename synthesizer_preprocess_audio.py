61e43dad1685266ddd6d69f6fc554081f01f3ccd
CorentinJ <corentin.jemine@gmail.com>
2021-12-28 11:35:38 +0100

    Updated synthesizer audio preprocessing

--- synthesizer_preprocess_audio.py
+++ synthesizer_preprocess_audio.py
@@ -17,21 +17,19 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--out_dir", type=Path, default=argparse.SUPPRESS, help=\
         "Path to the output directory that will contain the mel spectrograms, the audios and the "
         "embeds. Defaults to <datasets_root>/SV2TTS/synthesizer/")
-    parser.add_argument("-n", "--n_processes", type=int, default=None, help=\
+    parser.add_argument("-n", "--n_processes", type=int, default=4, help=\
         "Number of processes in parallel.")
     parser.add_argument("-s", "--skip_existing", action="store_true", help=\
         "Whether to overwrite existing files with the same name. Useful if the preprocessing was "
         "interrupted.")
     parser.add_argument("--hparams", type=str, default="", help=\
         "Hyperparameter overrides as a comma-separated list of name-value pairs")
-    parser.add_argument("--no_trim", action="store_true", help=\
-        "Preprocess audio without trimming silences (not recommended).")
     parser.add_argument("--no_alignments", action="store_true", help=\
         "Use this option when dataset does not include alignments\
         (these are used to split long audio files into sub-utterances.)")
     parser.add_argument("--datasets_name", type=str, default="LibriSpeech", help=\
         "Name of the dataset directory to process.")
-    parser.add_argument("--subfolders", type=str, default="train-clean-100, train-clean-360", help=\
+    parser.add_argument("--subfolders", type=str, default="train-clean-100,train-clean-360", help=\
         "Comma-separated list of subfolders to process inside your dataset directory")
     args = parser.parse_args()
 
@@ -42,16 +40,6 @@ if __name__ == "__main__":
     # Create directories
     assert args.datasets_root.exists()
     args.out_dir.mkdir(exist_ok=True, parents=True)
-
-    # Verify webrtcvad is available
-    if not args.no_trim:
-        try:
-            import webrtcvad
-        except:
-            raise ModuleNotFoundError("Package 'webrtcvad' not found. This package enables "
-                "noise removal and is recommended. Please install and try again. If installation fails, "
-                "use --no_trim to disable this error message.")
-    del args.no_trim
 
     # Preprocess the dataset
     print_args(args, parser)
