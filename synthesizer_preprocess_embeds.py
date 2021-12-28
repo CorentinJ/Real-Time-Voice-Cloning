95b0c10d97ad0967fc4f7d09e226c24e7fe7d661
CorentinJ <corentin.jemine@gmail.com>
2021-12-28 11:48:23 +0100

    Updated synthesizer training

--- synthesizer_preprocess_embeds.py
+++ synthesizer_preprocess_embeds.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     parser.add_argument("synthesizer_root", type=Path, help=\
         "Path to the synthesizer training data that contains the audios and the train.txt file. "
         "If you let everything as default, it should be <datasets_root>/SV2TTS/synthesizer/.")
-    parser.add_argument("-e", "--encoder_model_fpath", type=Path, 
-                        default="encoder/saved_models/pretrained.pt", help=\
+    parser.add_argument("-e", "--encoder_model_fpath", type=Path,
+                        default="saved_models/default/encoder.pt", help=\
         "Path your trained encoder model.")
     parser.add_argument("-n", "--n_processes", type=int, default=4, help= \
         "Number of parallel processes. An encoder is created for each, so you may need to lower "
         "this value on GPUs with low memory. Set it to 1 if CUDA is unhappy.")
     args = parser.parse_args()
-    
+
     # Preprocess the dataset
     print_args(args, parser)
-    create_embeddings(**vars(args))    
+    create_embeddings(**vars(args))
