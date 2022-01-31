39188b4ddda7fd20f834fa04e7a6cd8e4d73b8d3
PlayerXYT <48261758+PlayerXYT@users.noreply.github.com>
2022-01-31 12:16:21 -0500

    I can't code :(

--- install_requirements_windows.bat
+++ install_requirements_windows.bat
@@ -1,18 +1,18 @@
 python -m pip install --upgrade pip
 python -m pip install torch==1.10.2+cu102 torchaudio===0.10.2+cu102 -f https://download.pytorch.org/whl/cu102/torch_stable.html
-python -m pip installinflect==5.3.0
-python -m pip installlibrosa==0.8.1
-python -m pip installmatplotlib==3.5.1
-python -m pip installnumpy==1.20.3
-python -m pip installPillow==8.4.0
-python -m pip installPyQt5==5.15.6
-python -m pip installscikit-learn==1.0.2
-python -m pip installscipy==1.7.3
-python -m pip installsounddevice==0.4.3
-python -m pip installSoundFile==0.10.3.post1
-python -m pip installtqdm==4.62.3
-python -m pip installumap-learn==0.5.2
-python -m pip installUnidecode==1.3.2
-python -m pip installurllib3==1.26.7
-python -m pip installvisdom==0.1.8.9
-python -m pip installwebrtcvad==2.0.10
+python -m pip install inflect==5.3.0
+python -m pip install librosa==0.8.1
+python -m pip install matplotlib==3.5.1
+python -m pip install numpy==1.20.3
+python -m pip install Pillow==8.4.0
+python -m pip install PyQt5==5.15.6
+python -m pip install scikit-learn==1.0.2
+python -m pip install scipy==1.7.3
+python -m pip install sounddevice==0.4.3
+python -m pip install SoundFile==0.10.3.post1
+python -m pip install tqdm==4.62.3
+python -m pip install umap-learn==0.5.2
+python -m pip install Unidecode==1.3.2
+python -m pip install urllib3==1.26.7
+python -m pip install visdom==0.1.8.9
+python -m pip install webrtcvad==2.0.10
