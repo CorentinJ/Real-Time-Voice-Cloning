bfbaef5ccc20ce2a268e9222c9420c9841ad4990
Slava <slava.v.ivannikov@gmail.com>
2022-04-29 11:18:23 +0200

    schedule rework

--- synthesizer/models/tacotron_tweaked/hparams.py
+++ synthesizer/models/tacotron_tweaked/hparams.py
@@ -15,6 +15,13 @@ class HParams(object):
     def __repr__(self):
         return pprint.pformat(self.__dict__)
 
+    def get_max(self, x):
+        for _, _, max_step, _ in self.tts_schedule:
+            if x < max_step:
+                return max_step
+        print("you are overtraining man")
+        return x * 2
+
     def parse(self, string):
         # Overrides hparams from a comma-separated string of name=value pairs
         if len(string) > 0:
@@ -64,6 +71,15 @@ hparams = HParams(
                   (2, 1e-4, 160_000, 12),  # r = reduction factor (# of mel frames
                   (2, 3e-5, 320_000, 12),  # synthesized for each decoder iteration)
                   (2, 1e-5, 640_000, 12)],  # lr = learning rate
+
+    tts_schedule_dict={
+        20_000: (2, 1e-3, 20_000, 12),  # Progressive training schedule
+        40_000: (2, 5e-4, 40_000, 12),  # (r, lr, step, batch_size)
+        80_000: (2, 2e-4, 80_000, 12),  #
+        160_000: (2, 1e-4, 160_000, 12),  # r = reduction factor (# of mel frames
+        320_000: (2, 3e-5, 320_000, 12),  # synthesized for each decoder iteration)
+        640_000: (2, 1e-5, 640_000, 12)
+    },
 
     tts_clip_grad_norm=1.0,  # clips the gradient norm to prevent explosion - set to None if not needed
     tts_eval_interval=500,  # Number of steps between model evaluation (sample generation)
--- synthesizer/models/tacotron_tweaked/train.py
+++ synthesizer/models/tacotron_tweaked/train.py
@@ -136,196 +136,187 @@ def train(run_id: str, syn_dir: Path, models_dir: Path, save_every: int, backup_
 
     gradinit_bsize = int(batch_size / 2) if gradinit_bsize < 0 else int(gradinit_bsize / 2)
     scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
-    # if torch.cuda.device_count() > 1:
-    #     model = nn.DataParallel(model)
-    for i, session in enumerate(hparams.tts_schedule):
-        r, lr, max_step, batch_size = session
-        if force_restart or not weights_fpath.exists():
-            print("\nStarting the training of Tacotron from scratch\n")
-            gradinit_do = True
-            model.save(weights_fpath, optimizer)
+    if force_restart or not weights_fpath.exists():
+        print("\nStarting the training of Tacotron from scratch\n")
+        gradinit_do = True
+        model.save(weights_fpath, None)
 
-            # Embeddings metadata
-            char_embedding_fpath = meta_folder.joinpath("CharacterEmbeddings.tsv")
-            with open(char_embedding_fpath, "w", encoding="utf-8") as f:
-                for symbol in symbols:
-                    if symbol == " ":
-                        symbol = "\\s"  # For visual purposes, swap space with \s
+        # Embeddings metadata
+        char_embedding_fpath = meta_folder.joinpath("CharacterEmbeddings.tsv")
+        with open(char_embedding_fpath, "w", encoding="utf-8") as f:
+            for symbol in symbols:
+                if symbol == " ":
+                    symbol = "\\s"  # For visual purposes, swap space with \s
 
-                    f.write("{}\n".format(symbol))
+                f.write("{}\n".format(symbol))
 
-        else:
-            print("\nLoading weights at %s" % weights_fpath)
-            model.load(weights_fpath, optimizer)
-            print("Tacotron weights loaded from step %d" % model.step)
-            gradinit_do = False
-        print("using gradinit" if gradinit_do else "not using gradinit")
-        optimizer = optim.SGD(
-            [{'params': parameters_bias, 'lr': lr / 10.},
-             {'params': parameters_scale, 'lr': lr / 10.},
-             {'params': parameters_others}],
-            lr=lr * batch_size / 128.,
-            momentum=0.9,
-            weight_decay=wd)
+    else:
+        print("\nLoading weights at %s" % weights_fpath)
+        model.load(weights_fpath, None)
+        print("Tacotron weights loaded from step %d" % model.step)
+        gradinit_do = False
+    r, lr, max_step, batch_size = hparams.tts_schedule_dict[hparams.get_max(model.step)]
+    optimizer = optim.SGD(
+        [{'params': parameters_bias, 'lr': lr / 10.},
+         {'params': parameters_scale, 'lr': lr / 10.},
+         {'params': parameters_others}],
+        lr=lr * batch_size / 128.,
+        momentum=0.9,
+        weight_decay=wd)
+    if force_restart or not weights_fpath.exists():
+        model.save(weights_fpath, optimizer)
+    else:
+        model.load(weights_fpath, optimizer)
+    print("using gradinit" if gradinit_do else "not using gradinit")
 
-        current_step = model.get_step()
+    current_step = model.get_step()
 
-        training_steps = max_step - current_step
-        collate_fn = partial(collate_synthesizer, r=r, hparams=hparams)
-        gradinit_trainloader = DataLoader(dataset, gradinit_bsize, shuffle=True, num_workers=4, collate_fn=collate_fn,
-                                          pin_memory=True)
-        if gradinit_do:
-            model = gradinit(model, gradinit_trainloader, dataset, device, Struct(**args))
-        sgdr = CosineAnnealingLR(optimizer, n_epoch * len(gradinit_trainloader), eta_min=0, last_epoch=-1)
+    training_steps = max_step - current_step
+    collate_fn = partial(collate_synthesizer, r=r, hparams=hparams)
+    gradinit_trainloader = DataLoader(dataset, gradinit_bsize, shuffle=True, num_workers=4, collate_fn=collate_fn,
+                                      pin_memory=True)
+    if gradinit_do:
+        model = gradinit(model, gradinit_trainloader, dataset, device, Struct(**args))
+    sgdr = CosineAnnealingLR(optimizer, n_epoch * len(gradinit_trainloader), eta_min=0, last_epoch=-1)
 
-        # Do we need to change to the next session?
-        if current_step >= max_step:
-            # Are there no further sessions than the current one?
-            if i == len(hparams.tts_schedule) - 1:
-                # We have completed training. Save the model and exit
-                model.save(weights_fpath, optimizer)
+    if debug:
+        print("Training point 1", time.time() - use_time)
+        use_time = time.time()
+    model.r = r
+
+    # Begin the training
+    simple_table([(f"Steps with r={r}", str(training_steps // 1000) + "k Steps"),
+                  ("Batch Size", batch_size),
+                  ("Learning Rate", lr),
+                  ("Outputs/Step (r)", model.r)])
+
+    data_loader = DataLoader(dataset, batch_size, shuffle=True, num_workers=4, collate_fn=collate_fn,
+                             pin_memory=True)
+
+    total_iters = len(dataset)
+    steps_per_epoch = np.ceil(total_iters / batch_size).astype(np.int32)
+    epochs = np.ceil(training_steps / steps_per_epoch).astype(np.int32)
+    if debug:
+        print("Training point 2", time.time() - use_time)
+        use_time = time.time()
+    dt_len = len(data_loader)
+    print("Printing every", print_every, "steps")
+    for epoch in range(1, epochs + 1):
+        for i, (texts, mels, embeds, idx) in enumerate(data_loader, 1):
+            if perf_limit and i >= 500:  # leave this to me, should be 500
                 break
+            # print(texts)
+            torch.cuda.synchronize()
+            # print(texts, texts[0])
+            start_time = time.time()
+            if debug:
+                print("Training point 2.1", time.time() - use_time)
+                use_time = time.time()
+            # Generate stop tokens for training
+            stop = torch.ones(mels.shape[0], mels.shape[2], device=device)
+            for j, k in enumerate(idx):
+                stop[j, :int(dataset.metadata[k][4]) - 1] = 0
+
+            # print("t:", texts.shape)
+            # print("t s:", texts[random.randint(1, 4)][:10])
+            texts = texts.to(device)
+            mels = mels.to(device)
+            embeds = embeds.to(device)
+            # stop = stop.to(device)
+            if debug:
+                print("Training point 2.2", time.time() - use_time)
+                use_time = time.time()
+            # Forward pass
+            # Parallelize model onto GPUS using workaround due to python bug
+            # print(use_amp, bool(use_amp))
+            use_amp = bool(use_amp)
+            with autocast(enabled=use_amp):
+                m1_hat, m2_hat, attention, stop_pred = model(texts, mels, embeds)
+            if debug:
+                print("Training point 2.3", time.time() - use_time)
+                use_time = time.time()
+            # Backward pass
+            m1_loss = F.mse_loss(m1_hat, mels) + F.l1_loss(m1_hat, mels)
+            m2_loss = F.mse_loss(m2_hat, mels)
+            stop_loss = F.binary_cross_entropy(stop_pred, stop)
+
+            loss = m1_loss + m2_loss + stop_loss
+
+            # loss.backward()
+            if loss < 2:  # fsr it becomes inf sometimes
+                optimizer.zero_grad(set_to_none=True)
+                scaler.scale(loss).backward()
+                scaler.unscale_(optimizer)
+
+                if hparams.tts_clip_grad_norm is not None:
+                    grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), hparams.tts_clip_grad_norm)
+                    if np.isnan(grad_norm.cpu()):
+                        print("grad_norm was NaN!")
+
+                # optimizer.step()
+
+                scaler.step(optimizer)
+                scaler.update()
+                sgdr.step()
             else:
-                # There is a following session, go to it
-                continue
-        if debug:
-            print("Training point 1", time.time() - use_time)
-            use_time = time.time()
-        model.r = r
+                print(f"loss is {loss} fsr")
 
-        # Begin the training
-        simple_table([(f"Steps with r={r}", str(training_steps // 1000) + "k Steps"),
-                      ("Batch Size", batch_size),
-                      ("Learning Rate", lr),
-                      ("Outputs/Step (r)", model.r)])
+            time_window.append(time.time() - start_time)
+            loss_window.append(loss.item())
 
-        data_loader = DataLoader(dataset, batch_size, shuffle=True, num_workers=4, collate_fn=collate_fn,
-                                 pin_memory=True)
+            step = model.get_step()
+            k = step // 1000
 
-        total_iters = len(dataset)
-        steps_per_epoch = np.ceil(total_iters / batch_size).astype(np.int32)
-        epochs = np.ceil(training_steps / steps_per_epoch).astype(np.int32)
-        if debug:
-            print("Training point 2", time.time() - use_time)
-            use_time = time.time()
-        dt_len = len(data_loader)
-        print("Printing every", print_every, "steps")
-        for epoch in range(1, epochs + 1):
-            for i, (texts, mels, embeds, idx) in enumerate(data_loader, 1):
-                if perf_limit and i >= 500:  # leave this to me, should be 500
-                    break
-                # print(texts)
-                torch.cuda.synchronize()
-                # print(texts, texts[0])
-                start_time = time.time()
-                if debug:
-                    print("Training point 2.1", time.time() - use_time)
-                    use_time = time.time()
-                # Generate stop tokens for training
-                stop = torch.ones(mels.shape[0], mels.shape[2], device=device)
-                for j, k in enumerate(idx):
-                    stop[j, :int(dataset.metadata[k][4]) - 1] = 0
+            msg = {
+                "Loss": f"{loss_window.average:#.4}",
+                "steps/s": f"{1. / time_window.average:#.2}",
+                "Step": step,
+                "One step time": str(round(time.time() - start_time, 2)) + "s"
+            }
+            if i % print_every == 0:
+                dl_logger.log(step=(epoch, str(i) + "/" + str(dt_len)), data=msg)
+            # Backup or save model as appropriate
+            if backup_every != 0 and step % backup_every == 0:
+                backup_fpath = weights_fpath.parent / f"synthesizer_{k:06d}.pt"
+                model.save(backup_fpath, optimizer)
 
-                # print("t:", texts.shape)
-                # print("t s:", texts[random.randint(1, 4)][:10])
-                texts = texts.to(device)
-                mels = mels.to(device)
-                embeds = embeds.to(device)
-                # stop = stop.to(device)
-                if debug:
-                    print("Training point 2.2", time.time() - use_time)
-                    use_time = time.time()
-                # Forward pass
-                # Parallelize model onto GPUS using workaround due to python bug
-                # print(use_amp, bool(use_amp))
-                use_amp = bool(use_amp)
-                with autocast(enabled=use_amp):
-                    m1_hat, m2_hat, attention, stop_pred = model(texts, mels, embeds)
-                if debug:
-                    print("Training point 2.3", time.time() - use_time)
-                    use_time = time.time()
-                # Backward pass
-                m1_loss = F.mse_loss(m1_hat, mels) + F.l1_loss(m1_hat, mels)
-                m2_loss = F.mse_loss(m2_hat, mels)
-                stop_loss = F.binary_cross_entropy(stop_pred, stop)
+            if save_every != 0 and step % save_every == 0:
+                # Must save latest optimizer state to ensure that resuming training
+                # doesn't produce artifacts
+                model.save(weights_fpath, optimizer)
+                dl_logger.log("INFO", data={
+                    "status": "saved.."
+                })
+                dl_logger.flush()
 
-                loss = m1_loss + m2_loss + stop_loss
+            # Evaluate model to generate samples
+            epoch_eval = hparams.tts_eval_interval == -1 and i == steps_per_epoch  # If epoch is done
+            step_eval = hparams.tts_eval_interval > 0 and step % hparams.tts_eval_interval == 0  # Every N steps
+            if epoch_eval or step_eval:
+                for sample_idx in range(hparams.tts_eval_num_samples):
+                    # At most, generate samples equal to number in the batch
+                    if sample_idx + 1 <= len(texts):
+                        # Remove padding from mels using frame length in metadata
+                        mel_length = int(dataset.metadata[idx[sample_idx]][4])
+                        mel_prediction = np_now(m2_hat[sample_idx]).T[:mel_length]
+                        target_spectrogram = np_now(mels[sample_idx]).T[:mel_length]
+                        attention_len = mel_length // model.r
 
-                # loss.backward()
-                if loss < 2:  # fsr it becomes inf sometimes
-                    optimizer.zero_grad(set_to_none=True)
-                    scaler.scale(loss).backward()
-                    scaler.unscale_(optimizer)
+                        eval_model(attention=np_now(attention[sample_idx][:, :attention_len]),
+                                   mel_prediction=mel_prediction,
+                                   target_spectrogram=target_spectrogram,
+                                   input_seq=np_now(texts[sample_idx]),
+                                   step=step,
+                                   plot_dir=plot_dir,
+                                   mel_output_dir=mel_output_dir,
+                                   wav_dir=wav_dir,
+                                   sample_num=sample_idx + 1,
+                                   loss=loss,
+                                   hparams=hparams)
 
-                    if hparams.tts_clip_grad_norm is not None:
-                        grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), hparams.tts_clip_grad_norm)
-                        if np.isnan(grad_norm.cpu()):
-                            print("grad_norm was NaN!")
-
-                    # optimizer.step()
-
-                    scaler.step(optimizer)
-                    scaler.update()
-                    sgdr.step()
-                else:
-                    print(f"loss is {loss} fsr")
-
-                time_window.append(time.time() - start_time)
-                loss_window.append(loss.item())
-
-                step = model.get_step()
-                k = step // 1000
-
-                msg = {
-                    "Loss": f"{loss_window.average:#.4}",
-                    "steps/s": f"{1. / time_window.average:#.2}",
-                    "Step": step,
-                    "One step time": str(round(time.time() - start_time, 2)) + "s"
-                }
-                if i % print_every == 0:
-                    dl_logger.log(step=(epoch, str(i) + "/" + str(dt_len)), data=msg)
-                # Backup or save model as appropriate
-                if backup_every != 0 and step % backup_every == 0:
-                    backup_fpath = weights_fpath.parent / f"synthesizer_{k:06d}.pt"
-                    model.save(backup_fpath, optimizer)
-
-                if save_every != 0 and step % save_every == 0:
-                    # Must save latest optimizer state to ensure that resuming training
-                    # doesn't produce artifacts
-                    model.save(weights_fpath, optimizer)
-                    dl_logger.log("INFO", data={
-                        "status": "saved.."
-                    })
-                    dl_logger.flush()
-
-                # Evaluate model to generate samples
-                epoch_eval = hparams.tts_eval_interval == -1 and i == steps_per_epoch  # If epoch is done
-                step_eval = hparams.tts_eval_interval > 0 and step % hparams.tts_eval_interval == 0  # Every N steps
-                if epoch_eval or step_eval:
-                    for sample_idx in range(hparams.tts_eval_num_samples):
-                        # At most, generate samples equal to number in the batch
-                        if sample_idx + 1 <= len(texts):
-                            # Remove padding from mels using frame length in metadata
-                            mel_length = int(dataset.metadata[idx[sample_idx]][4])
-                            mel_prediction = np_now(m2_hat[sample_idx]).T[:mel_length]
-                            target_spectrogram = np_now(mels[sample_idx]).T[:mel_length]
-                            attention_len = mel_length // model.r
-
-                            eval_model(attention=np_now(attention[sample_idx][:, :attention_len]),
-                                       mel_prediction=mel_prediction,
-                                       target_spectrogram=target_spectrogram,
-                                       input_seq=np_now(texts[sample_idx]),
-                                       step=step,
-                                       plot_dir=plot_dir,
-                                       mel_output_dir=mel_output_dir,
-                                       wav_dir=wav_dir,
-                                       sample_num=sample_idx + 1,
-                                       loss=loss,
-                                       hparams=hparams)
-
-                # Break out of loop to update training schedule
-                if step >= max_step:
-                    break
+            # Break out of loop to update training schedule
+            if step >= max_step:
+                break
 
 
 def test(model, device, data_loader, dataset):
