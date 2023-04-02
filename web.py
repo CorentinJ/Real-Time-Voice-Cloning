bdf8534fc475391145ae0a5913abd87fd64cabc4
Stark <43443357+stark-eth@users.noreply.github.com>
2023-04-02 14:10:41 +0800

    Create web.py

---
+++ web.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import typer
+from pyngrok import ngrok
+
+cli = typer.Typer()
+
+
+@cli.command()
+def launch(port: int = typer.Option(2023, "--port", "-p")) -> None:
+    """Start a graphical UI server for the opyrator.
+
+    The UI is auto-generated from the input- and output-schema of the given function.
+    """
+    # Setup a tunnel to the streamlit port 2023
+    ngrok.kill()
+    public_url = ngrok.connect(2023)
+    print(public_url)
+    # Add the current working directory to the sys path
+    # This is required to resolve the opyrator path
+    sys.path.append(os.getcwd())
+
+    from control.mkgui.base.ui.streamlit_ui import launch_ui
+
+    launch_ui(port)
+    
+
+if __name__ == "__main__":
+    cli()
