d6f39de104744a80d19d75e50b4ae5ac0c4e5349
Kaled Dahleh <kdahleh23@gmail.com>
2024-07-22 21:34:24 -0500

    models updated

--- utils/default_models.py
+++ utils/default_models.py
@@ -1,17 +1,13 @@
-import urllib.request
+import gdown
 from pathlib import Path
 from threading import Thread
-from urllib.error import HTTPError
-
 from tqdm import tqdm
-
 
 default_models = {
     "encoder": ("https://drive.google.com/uc?export=download&id=1q8mEGwCkFy23KZsinbuvdKAQLqNKbYf1", 17090379),
-    "synthesizer": ("https://drive.google.com/u/0/uc?id=1EqFMIbvxffxtjiVrtykroF6_mUh-5Z3s&export=download&confirm=t", 370554559),
+    "synthesizer": ("https://drive.google.com/uc?export=download&id=1EqFMIbvxffxtjiVrtykroF6_mUh-5Z3s&export=download", 370554559),
     "vocoder": ("https://drive.google.com/uc?export=download&id=1cf2NO6FtI0jDuy8AV3Xgn6leO6dHjIgu", 53845290),
 }
-
 
 class DownloadProgressBar(tqdm):
     def update_to(self, b=1, bsize=1, tsize=None):
@@ -19,21 +15,14 @@ class DownloadProgressBar(tqdm):
             self.total = tsize
         self.update(b * bsize - self.n)
 
-
 def download(url: str, target: Path, bar_pos=0):
-    # Ensure the directory exists
     target.parent.mkdir(exist_ok=True, parents=True)
-
     desc = f"Downloading {target.name}"
-    with DownloadProgressBar(unit="B", unit_scale=True, miniters=1, desc=desc, position=bar_pos, leave=False) as t:
-        try:
-            urllib.request.urlretrieve(url, filename=target, reporthook=t.update_to)
-        except HTTPError:
-            return
 
+    with DownloadProgressBar(unit="B", unit_scale=True, miniters=1, desc=desc, position=bar_pos, leave=False) as t:
+        gdown.download(url, str(target), quiet=False)
 
 def ensure_default_models(models_dir: Path):
-    # Define download tasks
     jobs = []
     for model_name, (url, size) in default_models.items():
         target_path = models_dir / "default" / f"{model_name}.pt"
@@ -47,7 +36,6 @@ def ensure_default_models(models_dir: Path):
         thread.start()
         jobs.append((thread, target_path, size))
 
-    # Run and join threads
     for thread, target_path, size in jobs:
         thread.join()
 
